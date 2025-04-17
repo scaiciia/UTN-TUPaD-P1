64c9cf3b4433f94aeb27322b88d4d7f184dbe0bc
scaiciia <scaiciia@gmail.com>
2025-04-17 01:54:19 -0300

    Mod CAICIIA-MASSELLO_TP-03.py

--- 03 Estructuras Condicionales/CAICIIA-MASSELLO_TP-03.py
+++ 03 Estructuras Condicionales/CAICIIA-MASSELLO_TP-03.py
@@ -1,3 +1,4 @@
+# https://github.com/scaiciia/UTN-TUPaD-P1
 # 1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años, 
 #     deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
 
